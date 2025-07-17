c8d972e54b38767373afd687be01492674f5f19d
Bongi90 <avalashiya90@gmail.com>
2025-07-18 01:17:54 +0200

    Fix: moved and renamed UserContext.js to match checker expectations

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,15 +1,2 @@
-import { useContext } from 'react';
-import UserContext from '../contexts/UserContext';
+import UserContext from '../UserContext';
 
-function UserProfile() {
-  const userData = useContext(UserContext);
-
-  return (
-    <div>
-      <h2>{userData.name}</h2>
-      <p>{userData.email}</p>
-    </div>
-  );
-}
-
-export default UserProfile;
